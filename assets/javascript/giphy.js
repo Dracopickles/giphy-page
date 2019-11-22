4c6583cb4d5b9869ca2c07aef4431e4c67b164e2
Karl Lockbaum <jrlockbaum@sbcglobal.net>
2019-11-21 20:11:52 -0800

    got the buttons to spawn giphys now to make some disappear

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -19,4 +19,40 @@ function rendorButtons(){
   }
 }
 
+const dinoClick = function(){
+  let dinosaur = $(this).attr("dataName");
+  
+  let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
+  dinosaur + "&api_key=bdt2T4fEiBi60Hz8wOfbFA4m5aMAcC08&limit=10"
+
+  $.ajax({
+    url:queryURL,
+    method:"GET"
+  })
+   .then(function(response){
+     let results = response.data;
+
+     for (i = 0; i < results.length; i++) {
+
+     let dinoDiv = $("<div>");
+
+     let p = $("<p>").text("Rating: " + results[i].rating);
+
+     let dinoImage = $("<img>");
+
+     dinoImage.attr("src", results[i].images.fixed_height.url);
+
+     dinoDiv.append(p);
+     dinoDiv.append(dinoImage);
+
+     $("#dinosaur-view").prepend(dinoDiv);
+    }
+   });
+  };
+
 rendorButtons();
+// $(document).ready(function(){
+//   $("#button").on("click", dinoClick);
+// });
+
+$(document).on("click", ".movieBtn",dinoClick);
